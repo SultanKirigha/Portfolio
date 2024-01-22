479427aba8e2c2cad9e0cba74ed4d3dc9548cb7c
Sultan Kirigha <sultankirigha@gmail.com>
2024-01-22 18:40:37 +0300

    Have successfully linked the About section to Sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import './About.css'
-
+import { urlFor, client } from '../../client';
  
-const abouts = [
-  {title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-  {title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
-  {title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03},
-  {title: 'Graphic Design', description: 'I am a good web developer.', imgUrl: images.about04}
-
-
-
-]
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
@@ -28,7 +28,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
